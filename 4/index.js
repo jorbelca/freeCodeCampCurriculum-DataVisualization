97c1f2457f69ba38154658a2ecefa500eafa7e41
jorbelca <76847923+jorbelca@users.noreply.github.com>
2023-02-02 08:55:11 +0100

    Add files via upload

--- 4/index.js
+++ 4/index.js
@@ -13,114 +13,99 @@ let margin = { top: 20, bottom: 20, left: 20, right: 20 }
 
 
 
-fetch(apiEndPointEducation)
-  .then(
-    function (response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
 
-      // response
-      response.json()
-        .then(function (data) {
-          console.log(data);
-          allData = data
+async function choroplethMap() {
+  try {
+
+    const svg = d3.select("body")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
+    const color = d3.scaleSequential().interpolator(d3.interpolateViridis)
+      .domain([2.6, 75.1])
+
+    const educationData = await d3.json(apiEndPointEducation);
+
+    const mapData = await d3.json(apiEndPointCountyData);
+    console.log(mapData);
+
+    const path = d3.geoPath();
+
+    
+    svg.append("g")
+      .selectAll("path")
+      .data(topojson.feature(mapData, mapData.objects.counties).features)
+      .enter().append("path").attr("d", path)
+      .attr("class", "county")
+      .attr("data-fips", d => d.id)
+      .attr("data-education", d => (educationData.filter(e => e.fips == d.id))[0].bachelorsOrHigher)
+      .attr("fill", d => color((educationData.filter(e => e.fips == d.id))[0].bachelorsOrHigher))
 
 
-
-          const svg = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-
-
-
-          // Map and projection
-          const projection = d3.geoMercator()
-            .center([-102, 37])                // GPS of location to zoom on
-            .scale(700)                       // This is like the zoom
-            .translate([width / 2, height / 2])
-
-          // Load external data and boot
-          d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function (data) {
-
-            // Filter data
-            data.features = data.features.filter(d => { console.log(d.properties.name); return d.properties.name == "USA" })
-
-            // Draw the map
-            svg.append("g")
-              .selectAll("path")
-              .data(data.features)
-              .join("path")
-              .attr("fill", "grey")
-              .attr("d", d3.geoPath()
-                .projection(projection)
-              )
-              .style("stroke", "none")
-
-            // // Tooltip
-            // var Tooltip = d3.select("body")
-            //   .append("div")
-            //   .attr('id', 'tooltip')
-            //   .style("visibility", "hidden")
-            //   .style('width', 'auto')
-            //   .style('heigth', 'auto')
-            //   .style("background-color", "white")
-            //   .style("border", "solid")
-            //   .style("border-width", "2px")
-            //   .style("border-radius", "5px")
-            //   .style("padding", "5px")
+    // // Tooltip
+    // var Tooltip = d3.select("body")
+    //   .append("div")
+    //   .attr('id', 'tooltip')
+    //   .style("visibility", "hidden")
+    //   .style('width', 'auto')
+    //   .style('heigth', 'auto')
+    //   .style("background-color", "white")
+    //   .style("border", "solid")
+    //   .style("border-width", "2px")
+    //   .style("border-radius", "5px")
+    //   .style("padding", "5px")
 
 
 
 
 
 
-            // .style("fill", function (d) {
-            //   let variance = d.variance
-            //   if (variance <= -1) {
-            //     return 'Black'
-            //   } else if (variance <= 0) {
-            //     return 'Blue'
-            //   }
-            //   else if (variance <= 1) {
-            //     return 'Orange'
-            //   } else {
-            //     return 'Red'
-            //   }
+    // .style("fill", function (d) {
+    //   let variance = d.variance
+    //   if (variance <= -1) {
+    //     return 'Black'
+    //   } else if (variance <= 0) {
+    //     return 'Blue'
+    //   }
+    //   else if (variance <= 1) {
+    //     return 'Orange'
+    //   } else {
+    //     return 'Red'
+    //   }
 
 
-            // })
-            // .attr('transform', `translate(70,0)`)
+    // })
+    // .attr('transform', `translate(70,0)`)
 
-            // .on("mouseover", (item, idx) => {
-            //   Tooltip.transition()
-            //     .style('visibility', 'visible')
-            //   Tooltip.html(`
-            //     <div>
-            //     Year: ${idx.year} <br>
-            //     <p>Average Temp: ${title + idx.variance}</p> 
-            //     <p>Deviation: ${idx.variance}</p> 
-            //     </div>  
-            //   `)
-            //   Tooltip.attr("data-year", () => idx.year)
+    // .on("mouseover", (item, idx) => {
+    //   Tooltip.transition()
+    //     .style('visibility', 'visible')
+    //   Tooltip.html(`
+    //     <div>
+    //     Year: ${idx.year} <br>
+    //     <p>Average Temp: ${title + idx.variance}</p> 
+    //     <p>Deviation: ${idx.variance}</p> 
+    //     </div>  
+    //   `)
+    //   Tooltip.attr("data-year", () => idx.year)
 
-            // })
-            // .on("mouseout", () => Tooltip.transition().style('visibility', 'hidden'))
-
-
+    // })
+    // .on("mouseout", () => Tooltip.transition().style('visibility', 'hidden'))
 
 
-          }
-          )
-        })
-    })
 
 
-  .catch(function (err) {
-    console.log('Fetch Error :-S', err);
-  });
+
+
+
+
+
+
+  } catch (error) { console.log(error.name, error.message, error) }
+
+}
+
+choroplethMap()
 
 
