c55b596b7987ed157c990e40cbb2f92f928a3e69
jorbelca <76847923+jorbelca@users.noreply.github.com>
2023-02-02 12:13:49 +0100

    Add files via upload

--- 4/index.js
+++ 4/index.js
@@ -26,13 +26,26 @@ async function choroplethMap() {
       .domain([2.6, 75.1])
 
     const educationData = await d3.json(apiEndPointEducation);
-
     const mapData = await d3.json(apiEndPointCountyData);
-    console.log(mapData);
 
     const path = d3.geoPath();
 
-    
+
+    // Tooltip
+    var Tooltip = d3.select("body")
+      .append("div")
+      .attr('id', 'tooltip')
+      .style("visibility", "hidden")
+      .style('width', 'auto')
+      .style('heigth', 'auto')
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+
+
+
     svg.append("g")
       .selectAll("path")
       .data(topojson.feature(mapData, mapData.objects.counties).features)
@@ -41,20 +54,21 @@ async function choroplethMap() {
       .attr("data-fips", d => d.id)
       .attr("data-education", d => (educationData.filter(e => e.fips == d.id))[0].bachelorsOrHigher)
       .attr("fill", d => color((educationData.filter(e => e.fips == d.id))[0].bachelorsOrHigher))
+      .on("mouseover", (item, idx) => {
+        Tooltip.transition()
+          .style('visibility', 'visible')
+        Tooltip.html(`
+        <div>
+       ${(educationData.filter(e => e.fips == idx.id))[0].area_name} : ${(educationData.filter(e => e.fips == idx.id))[0].bachelorsOrHigher} % 
+        </div>  
+      `)
+        Tooltip.attr("data-education", () =>
+          (educationData.filter(e => e.fips == idx.id))[0].bachelorsOrHigher)
+
+      })
+      .on("mouseout", () => Tooltip.transition().style('visibility', 'hidden'))
 
 
-    // // Tooltip
-    // var Tooltip = d3.select("body")
-    //   .append("div")
-    //   .attr('id', 'tooltip')
-    //   .style("visibility", "hidden")
-    //   .style('width', 'auto')
-    //   .style('heigth', 'auto')
-    //   .style("background-color", "white")
-    //   .style("border", "solid")
-    //   .style("border-width", "2px")
-    //   .style("border-radius", "5px")
-    //   .style("padding", "5px")
 
 
 
@@ -75,23 +89,6 @@ async function choroplethMap() {
     //   }
 
 
-    // })
-    // .attr('transform', `translate(70,0)`)
-
-    // .on("mouseover", (item, idx) => {
-    //   Tooltip.transition()
-    //     .style('visibility', 'visible')
-    //   Tooltip.html(`
-    //     <div>
-    //     Year: ${idx.year} <br>
-    //     <p>Average Temp: ${title + idx.variance}</p> 
-    //     <p>Deviation: ${idx.variance}</p> 
-    //     </div>  
-    //   `)
-    //   Tooltip.attr("data-year", () => idx.year)
-
-    // })
-    // .on("mouseout", () => Tooltip.transition().style('visibility', 'hidden'))
 
 
 
